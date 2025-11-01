ac25b6c18daefeb865433578cde47326689c8189
Zarmeen <zarmeennadeem211106@gmail.com>
2025-11-01 11:30:29 -0700

    Add files via upload

---
+++ 2 program.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Header file: #include<iostream>"<<endl;
+	cout<<"Namespace: using namespace std;"<<endl;
+	cout<<"Main fuction: intmain()"<<endl;
+	cout<<"Program body: program statements;"<<endl;
+	cout<<"Return: return 0;"<<endl;
+	return 0;
+}
---
+++ 3 program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+ using namespace std;
+int main()
+{
+	cout<<"**********************************"<<endl;
+	cout<<"*      STUDENT ID CARD           *"<<endl;
+	cout<<"**********************************"<<endl;
+	cout<<"Student Name:Zarmeen Nadeem"<<endl;
+	cout<<"Roll Number:11810"<<endl;
+	cout<<"Class:ADP-CS 1st Semester"<<endl;
+	cout<<"Subject:Programming Fundamentals"<<endl;
+	cout<<"Teacher:Mr. Hafiz Muhammad Usman"<<endl;
+	cout<<"**********************************"<<endl;
+	return 0;
+}

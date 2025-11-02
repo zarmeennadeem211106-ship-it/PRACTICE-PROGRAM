43732b44cb4fec9ee709c8db59ba04adff1be4d1
Zarmeen <zarmeennadeem211106@gmail.com>
2025-11-02 00:46:42 -0700

    Add files via upload

---
+++ 5 program.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"________________________________________________"<<endl;
+	
+	
+	cout<<"     UNDERSTANDING C++ PROGRAM STRUCTURE        "<<endl;
+	
+	cout<<"________________________________________________"<<endl;
+	
+	cout<<" 1.  Preprocessor Directive :"<<endl;
+	cout<<"     Example: #include<iostream>"<<endl;
+	cout<<"     Purpose: Include input/output stream library"<<endl;
+	
+	cout<<" 2.  Namespace Declaration :"<<endl;
+	cout<<"     Example: using namespace std;"<<endl;
+	cout<<"     Purpose: Allow use of standard library without std"<<endl;
+	
+	cout<<" 3.   Main Function:"<<endl;
+	cout<<"      Example: int main()"<<endl;
+	cout<<"      Purpose: Entry point of the program"<<endl;
+	
+	cout<<" 4.  Program Body: "<<endl;
+	cout<<"     Example: cout<< Hello World!;"<<endl;
+	cout<<"     Purpose: Contain the actual program logic"<<endl;
+	
+	cout<<" 5.  Return Statement:"<<endl;
+	cout<<"     Example: return 0n;"<<endl;
+	cout<<"     Purpose: Indicate successful program completion"<<endl;
+	
+	cout<<"__________________________________________________"<<endl;
+      	return 0;
+}

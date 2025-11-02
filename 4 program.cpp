053912c3edd0f3b310fc924bf1977167b2d6e38d
Zarmeen <zarmeennadeem211106@gmail.com>
2025-11-02 00:03:48 -0700

    Add files via upload

---
+++ 4 program.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Welcome to Programming Fundamentals!"<<endl;
+	cout<<"Course Details:"<<endl;
+	cout<<"----------------------"<<endl;
+	cout<<"----------------------"<<endl;
+	cout<<"Course: Programming Fundamentals (PF)"<<endl;
+	cout<<"Duration: 16 Weeks"<<endl;
+	cout<<"Instructor: Mr.Hafiz Muhammad Usman"<<endl;
+	cout<<"Institute: Riphah International University"<<endl;
+    cout<<"                                     "	<<endl;
+	
+	cout<<"Good luck with your programming journey!"<<endl;
+	cout<<"-----------------------"<<endl;
+	return 0;
+}

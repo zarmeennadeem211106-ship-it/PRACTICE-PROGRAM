be24d9febd9a85ee9df8ffcd1b5f53eb02cb3824
Zarmeen <zarmeennadeem211106@gmail.com>
2025-11-04 18:23:55 -0800

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Name: Zarmeen Nadeem"<<endl;
+	cout<<"Roll No.: 11810"<<endl;
+	cout<<"Subject: Programming Fundamental";
+	return 0;
+
+}
---
+++ Untitled3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=15;
+	int b=20;
+	int c;
+	c=a+b;
+	cout<<"the sum is:"<<c;
+	return 0;
+
+}
